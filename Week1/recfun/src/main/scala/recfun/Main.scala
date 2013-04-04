3c428ea613f8746c028892b4c9994c35a066d6f0
Kinisoftware <kinisoftware@gmail.com>
2013-04-04 22:00:17 +0200

    Week1 Assignments

--- Week1/recfun/src/main/scala/recfun/Main.scala
+++ Week1/recfun/src/main/scala/recfun/Main.scala
@@ -21,10 +21,31 @@ object Main {
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def balance_iter(parentheses_opened: Int, chars: List[Char]): Boolean = {
+      if (chars.isEmpty) {
+        parentheses_opened == 0
+      } else {
+        if (chars.head.equals('('))
+          balance_iter(parentheses_opened + 1, chars.tail)
+        else if (chars.head.equals(')') && parentheses_opened == 0)
+          false
+        else if (chars.head.equals(')'))
+          balance_iter(parentheses_opened - 1, chars.tail)
+        else
+          balance_iter(parentheses_opened, chars.tail)
+      }
+    }
+
+    balance_iter(0, chars);
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (coins.isEmpty || money < 0) 0
+    else if (money > 0) countChange(money - coins.head, coins) + countChange(money, coins.tail)
+    else 1
+  }
 }
--- Week1/recfun/src/test/scala/recfun/BalanceSuite.scala
+++ Week1/recfun/src/test/scala/recfun/BalanceSuite.scala
@@ -24,4 +24,5 @@ class BalanceSuite extends FunSuite {
   test("balance: counting is not enough") {
     assert(!balance("())(".toList))
   }
+
 }
